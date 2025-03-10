1aceeceac6c196add61c8bd4bb3419001bee0c14
Aghogho Emerson Emokiniovo <aghoghoemerson7@gmail.com>
2025-03-10 17:51:37 +0100

    Update views.py

--- todoapp/views.py
+++ todoapp/views.py
@@ -11,11 +11,11 @@ from .models import todo
 def home(request):
     if request.method=="POST":
         task=request.POST.get("task")
-	if len(task) < 1:
-	    messages.error(request, "Please input a task")
-	    return redirect('')
-	    new_todo=todo(user=request.user, todo_name=task)
-	    new_todo.save()
+	    if len(task) < 1:
+	    	messages.error(request, "Please input a task")
+	    	return redirect('')
+		new_todo=todo(user=request.user, todo_name=task)
+		new_todo.save()
         
     all_todos=todo.objects.filter(user=request.user)
     context={
