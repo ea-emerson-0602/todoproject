e3587c75d397bec0761ef7ae31f49a9acaa8fd1e
Aghogho Emerson Emokiniovo <aghoghoemerson7@gmail.com>
2025-03-26 22:20:53 +0100

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'todoproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+                f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+        conn_max_age=600,  # Keep connections open for 10 minutes
+    )
 }
 
 
